6479bf8b57acd133e9a8d4ec29b9c29e59df6a2f
SunnyRx <admin@sunnyrx.com>
2023-03-15 23:04:58 +0900

    更改左上角 svg 图标

--- components/Header.js
+++ components/Header.js
@@ -70,32 +70,14 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="24"
-                    height="24"
-                    className="fill-current text-black dark:text-white"
-                  />
-                  <rect width="24" height="24" fill="url(#paint0_radial)" />
+                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <defs>
-                    <radialGradient
-                      id="paint0_radial"
-                      cx="0"
-                      cy="0"
-                      r="1"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="rotate(45) scale(39.598)"
-                    >
-                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                    </radialGradient>
+                    <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
+                      <stop offset="0%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+                      <stop offset="100%" style="stop-color:rgb(0,0,0);stop-opacity:1" />
+                    </linearGradient>
                   </defs>
+                  <rect width="24" height="24" fill="url(#grad)" />
                 </svg>
               </div>
             </a>
