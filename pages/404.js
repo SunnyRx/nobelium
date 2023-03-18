8b9deb40e7fddb728505c444ecfe4741970e802b
SunnyRx <admin@sunnyrx.com>
2023-03-19 00:48:54 +0900

    fix 404.js bug

--- pages/404.js
+++ pages/404.js
@@ -1,22 +1,31 @@
-import Container from '@/components/Container'
+import { useEffect } from 'react';
+import Container from '@/components/Container';
 
 export default function Custom404 () {
-  // 定义正则表达式
-  var regex = /^https?:\/\/sunnyrx\.com\/.*?\/([^\/]+)\/?$/;
+  const isBrowser = typeof window !== 'undefined';
 
-  // 检查当前页面的URL是否匹配正则表达式
-  var match = regex.exec(window.location.href);
-  if (match) {
-    // 如果匹配，重定向到新的URL
-    var newUrl = "https://sunnyrx.com/" + match[1];
-    window.location.replace(newUrl);
-    return null;
-  } else {
-    // 如果不匹配，显示404页面
+  useEffect(() => {
+    if (isBrowser) {
+      // 定义正则表达式
+      var regex = /^https?:\/\/sunnyrx\.com\/.*?\/([^\/]+)\/?$/;
+
+      // 检查当前页面的URL是否匹配正则表达式
+      var match = regex.exec(window.location.href);
+      if (match) {
+        // 如果匹配，重定向到新的URL
+        var newUrl = "https://sunnyrx.com/" + match[1];
+        window.location.replace(newUrl);
+      }
+    }
+  }, [isBrowser]);
+
+  if (!isBrowser) {
     return (
       <Container>
         <p style={{ textAlign: 'center', fontSize: '3em' }}>404 Not Found</p>
       </Container>
-    )
+    );
   }
+
+  return null;
 }
